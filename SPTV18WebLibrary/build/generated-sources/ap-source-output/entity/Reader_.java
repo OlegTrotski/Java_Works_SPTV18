008db002320f0c0ecfe31436e04454250b23b58e
OlegTrotski <54793780+OlegTrotski@users.noreply.github.com>
2020-03-05 11:01:58 +0200

    Add files via upload

---
+++ SPTV18WebLibrary/build/generated-sources/ap-source-output/entity/Reader_.java
@@ -0,0 +1,16 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-05T10:20:28")
+@StaticMetamodel(Reader.class)
+public class Reader_ { 
+
+    public static volatile SingularAttribute<Reader, String> firstname;
+    public static volatile SingularAttribute<Reader, String> phone;
+    public static volatile SingularAttribute<Reader, Long> id;
+    public static volatile SingularAttribute<Reader, String> lastname;
+
+}
